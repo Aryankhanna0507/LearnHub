51ceb8b4f90c848293e80875a8a7fd8431a65c74
Aryan <khannaaryan460@gmail.com>
2026-06-15 18:27:09 +0530

    add auth apis

--- client/src/feature/api/authApi.js
+++ client/src/feature/api/authApi.js
@@ -1,12 +1,12 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react"
-import { userLoggedIn } from "../authSlice"
+import { userLoggedIn, userLoggedOut } from "../authSlice"
 
 // backend base url
 const USER_API = "http://localhost:8080/api/v1/user/"
 
 export const authApi = createApi({
   reducerPath: "authApi",
-
+  tagTypes: ["User"],
   // base settings for all requests
   baseQuery: fetchBaseQuery({
     baseUrl: USER_API,
@@ -33,29 +33,56 @@ export const authApi = createApi({
       }),
 
       // user saved in redux after login success 
-      async onQueryStarted(arg, { queryFulfilled, dispatch }) {
+      async onQueryStarted(_, { queryFulfilled, dispatch }) {
+        try {
+          const result = await queryFulfilled
+          dispatch(userLoggedIn({ user: result.data.user }))
+          dispatch(authApi.util.invalidateTags(["User"]));
+        } catch (error) {
+          console.log(error)
+        }
+      }
+    }),
+    logoutUser: builder.mutation({
+      query: () => ({
+        url: "logout",
+        method: "GET",
+      }),
+      async onQueryStarted(_, { queryFulfilled, dispatch }) {
+        try {
+         await queryFulfilled
+          dispatch(userLoggedOut());
+          dispatch(authApi.util.invalidateTags(["User"]));
+        } catch (error) {
+          console.log(error);
+        }
+      },
+    }),
+    loadUser: builder.query({
+      query: () => ({
+        url: "profile",
+        method: "GET"
+      }),
+       providesTags: ["User"],
+      async onQueryStarted(_, { queryFulfilled, dispatch }) {
         try {
           const result = await queryFulfilled
           dispatch(userLoggedIn({ user: result.data.user }))
         } catch (error) {
-          console.log("Login failed")
+          console.log(error)
         }
       }
+
     }),
-    loadUser:builder.query({
-      query:()=>({
-        url:"profile",
-        method:"GET"
+    updateUser: builder.mutation({
+      query: (formData) => ({
+        url: "profile/update",
+        method: "PUT",
+        body: formData,
+        credentials: "include"
       })
     }),
-    updateUser:builder.mutation({
-      query:(formData)=>({
-        url:"profile/update",
-        method:"PUT",
-        body:formData,
-        credentials:"include"
-      })
-    })
+
   })
 })
 
@@ -63,5 +90,6 @@ export const {
   useRegisterUserMutation,
   useLoginUserMutation,
   useLoadUserQuery,
-  useUpdateUserMutation
+  useUpdateUserMutation,
+  useLogoutUserMutation
 } = authApi
